9e5d4ce2e6b06d326042e5efe9f892496f57e93c
tyrlafaye <fayelatyr61@gmail.com>
2026-05-04 23:21:53 +0200

    fix contact form no redirect

--- script.js
+++ script.js
@@ -123,36 +123,6 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
 
-
-const contactForm = document.getElementById("contactForm");
-
-if (contactForm) {
-  contactForm.addEventListener("submit", function (e) {
-    e.preventDefault();
-
-    const formData = new FormData(contactForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString()
-    })
-    .then(() => {
-      window.location.href = "/merci/index.html";
-    })
-    .catch((error) => {
-      alert("Erreur lors de l’envoi. Réessayez.");
-      console.error(error);
-    });
-  });
-}
-
-
-
-
-
-
-
 const contactForm = document.getElementById("contactForm");
 const formMessage = document.getElementById("formMessage");
 
